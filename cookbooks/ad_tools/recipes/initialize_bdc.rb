40f31015b8a9bd0f0ec4d21a93936f55b405d809
Ryan Geyer <rgeyer@its.jnj.com>
2010-11-17 12:01:01 -0800

    Look for the NTDS directory instead of a node attribute to determine whether or not to run DCPromo

--- cookbooks/ad_tools/recipes/initialize_bdc.rb
+++ cookbooks/ad_tools/recipes/initialize_bdc.rb
@@ -1,28 +1,23 @@
 log "Hostname Set is #{@node[:mnt_utils_hostname_set]}"
 log "DNS Set is #{@node[:mnt_utils_dns_set]}"
-log "BDC Init is #{@node[:ad_tools_is_bdc]}"
+log "NTDS exists is #{File.directory? "C:\\Windows\\NTDS"}"
 log "Should run this script is #{@node[:mnt_utils_hostname_set] && @node[:mnt_utils_dns_set]}"
 
-unless @node[:ad_tools_is_bdc]
+if(@node[:mnt_utils_hostname_set] && @node[:mnt_utils_dns_set] && !File.directory? "C:\\Windows\\NTDS")
+  log "Entered if other scripts run"
 
-  log "Entered unless :ad_tools_bdc_initialized"
-
-  if(@node[:mnt_utils_hostname_set] && @node[:mnt_utils_dns_set])
-    log "Entered if other scripts run"
-    
-    ad_tools_init_bdc "Create answers file, run dcpromo with it, then delete it" do
-      admin_user @node[:ad_tools][:admin_user]
-      admin_domain @node[:ad_tools][:admin_domain]
-      admin_pass @node[:ad_tools][:admin_pass]
-      domain_name @node[:ad_tools][:domain_name]
-      action :bdc
-    end
-
-    right_link_tag "ad:bdc=#{@node[:ad_tools][:domain_name]}"
-
-    @node[:ad_tools_is_bdc] = true
-
-    # We're going to let DCPromo issue the reboot request
-    # include_recipe 'utilities::system_reboot'
+  ad_tools_init_bdc "Create answers file, run dcpromo with it, then delete it" do
+    admin_user @node[:ad_tools][:admin_user]
+    admin_domain @node[:ad_tools][:admin_domain]
+    admin_pass @node[:ad_tools][:admin_pass]
+    domain_name @node[:ad_tools][:domain_name]
+    action :bdc
   end
+
+  right_link_tag "ad:bdc=#{@node[:ad_tools][:domain_name]}"
+
+  #@node[:ad_tools_is_bdc] = true
+
+  # We're going to let DCPromo issue the reboot request
+  # include_recipe 'utilities::system_reboot'
 end
