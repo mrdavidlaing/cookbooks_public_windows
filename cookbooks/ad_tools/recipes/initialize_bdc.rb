36a341e19c998d76babb4ff70b790e26df21daed
Ryan Geyer <rgeyer@its.jnj.com>
2010-11-18 14:41:50 -0800

    Syntax Tweaks for Join DC

--- cookbooks/ad_tools/recipes/initialize_bdc.rb
+++ cookbooks/ad_tools/recipes/initialize_bdc.rb
@@ -6,10 +6,10 @@ ntds_dir = "C:\\Windows\\NTDS"
 
 log "Hostname Set is #{@node[:mnt_utils_hostname_set]}"
 log "DNS Set is #{@node[:mnt_utils_dns_set]}"
-log "NTDS exists is #{default[:ad_bdc_initialized]}"
+#log "NTDS exists is #{default[:ad_bdc_initialized]}"
 log "Should run this script is #{@node[:mnt_utils_hostname_set] && @node[:mnt_utils_dns_set]}"
 
-if(@node[:mnt_utils_hostname_set] && @node[:mnt_utils_dns_set] && default[:ad_bdc_initialized])
+if(@node[:mnt_utils_hostname_set] && @node[:mnt_utils_dns_set] && !File.directory? ntds_dir)
   log "Entered if other scripts run"
   answers_file = "C:\\answers.txt"
 
@@ -17,24 +17,24 @@ if(@node[:mnt_utils_hostname_set] && @node[:mnt_utils_dns_set] && default[:ad_bd
     source "answers_bdc.txt.erb"
   end
 
-#  ad_tools_ad "Promote Server To BDC" do
-#    :unattended_dcpromo
-#  end
-
-  powershell "Promote BDC" do
-    powershell_script = <<'POWERSHELL_SCRIPT'
-    start-process -FilePath "$env:windir\Sysnative\dcpromo.exe" -ArgumentList /answer:C:\answers.txt -Wait
-
-    del "C:\answers.txt"
-POWERSHELL_SCRIPT
-
-    source(powershell_script)
+  ad_tools_ad "Promote Server To BDC" do
+    :unattended_dcpromo
   end
+
+#  powershell "Promote BDC" do
+#    powershell_script = <<'POWERSHELL_SCRIPT'
+#    start-process -FilePath "$env:windir\Sysnative\dcpromo.exe" -ArgumentList /answer:C:\answers.txt -Wait
+#
+#    del "C:\answers.txt"
+#POWERSHELL_SCRIPT
+#
+#    source(powershell_script)
+#  end
 
   right_link_tag "ad:domain=#{@node[:ad_tools][:domain_name]}"
   right_link_tag "ad:role=bdc"
 
-  default[:ad_bdc_initialized] = true
+  #default[:ad_bdc_initialized] = true
 
   #@node[:ad_tools_is_bdc] = true
 end
