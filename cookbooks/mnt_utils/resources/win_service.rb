3e8a1da9e3a7893a416621539fd9b4d7ec2173b9
Ryan Geyer <rgeyer@its.jnj.com>
2010-11-19 11:40:43 -0800

    Nuking the powershell provider & resource to try to fix syntax

--- cookbooks/mnt_utils/resources/win_service.rb
+++
@@ -1,6 +0,0 @@
-actions :set_service_account
-
-attribute :service_name, :kind_of => [String]
-attribute :service_account_user, :kind_of => [String]
-attribute :service_account_pass, :kind_of => [String]
-attribute :restart_service, :kind_of => [Boolean]
