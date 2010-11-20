062daa7bf59c3ef250276871f237b6b86fb7eff9
Ryan Geyer <rgeyer@its.jnj.com>
2010-11-19 21:13:13 -0800

    Syntax..

--- cookbooks/mnt_utils/powershell_providers/win32_svc/set_logon_account.ps1
+++ cookbooks/mnt_utils/powershell_providers/win32_svc/set_logon_account.ps1
@@ -4,7 +4,7 @@ $service_account_pass = Get-NewResource service_account_pass
 $restart_service = Get-NewResource restart_service
 
 # If service account user & pass are blank, we set it to $null, meaning that the Local System account will be used
-if ( ($service_account_user == "") -and ($service_account_pass == "") ) {
+if ( ($service_account_user -eq "") -and ($service_account_pass -eq "") ) {
   $service_account_user = $null
   $service_account_pass = $null
 }
@@ -12,6 +12,6 @@ if ( ($service_account_user == "") -and ($service_account_pass == "") ) {
 $service="name='$service_name'"
 
 $svc=gwmi win32_service -filter $service
-if ($restart_service.ToLower() == "true") { $svc.StopService() }
+if ($restart_service.ToLower() -eq "true") { $svc.StopService() }
 $svc.change($null,$null,$null,$null,$null,$null,$account,$password,$null,$null,$null)
-if ($restart_service.ToLower() == "true") { $svc.StartService() }
+if ($restart_service.ToLower() -eq "true") { $svc.StartService() }
