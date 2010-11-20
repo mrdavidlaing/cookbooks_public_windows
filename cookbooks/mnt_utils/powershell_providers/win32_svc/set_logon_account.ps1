3cd7738b93944ba35b8057451ea84de0a8494ba5
Ryan Geyer <rgeyer@its.jnj.com>
2010-11-20 10:13:51 -0800

    Null is already null

--- cookbooks/mnt_utils/powershell_providers/win32_svc/set_logon_account.ps1
+++ cookbooks/mnt_utils/powershell_providers/win32_svc/set_logon_account.ps1
@@ -4,10 +4,10 @@ $service_account_pass = Get-NewResource service_account_pass
 $restart_service = Get-NewResource restart_service
 
 # If service account user & pass are blank, we set it to $null, meaning that the Local System account will be used
-if ( ($service_account_user -eq "") -and ($service_account_pass -eq "") ) {
-  $service_account_user = $null
-  $service_account_pass = $null
-}
+#if ( ($service_account_user -eq "") -and ($service_account_pass -eq "") ) {
+#  $service_account_user = $null
+#  $service_account_pass = $null
+#}
 
 $service="name='$service_name'"
 
