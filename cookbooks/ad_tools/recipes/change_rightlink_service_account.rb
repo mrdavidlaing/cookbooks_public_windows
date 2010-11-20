1edb777c67ce116aaa2c2dc139409342ab831791
Ryan Geyer <rgeyer@its.jnj.com>
2010-11-19 17:56:37 -0800

    Let's use the right action, shall we?

--- cookbooks/ad_tools/recipes/change_rightlink_service_account.rb
+++ cookbooks/ad_tools/recipes/change_rightlink_service_account.rb
@@ -1,11 +1,11 @@
 mnt_utils_win32_svc "Set RightScale RightLink service account to Local System" do
   service_name "RightScale RightLink"
   restart_service "false"
-  action :logon_account
+  action :set_logon_account
 end
 
 mnt_utils_win32_svc "Set RightScale Level Manager service account to Local System" do
   service_name "RightScale Level Manager"
   restart_service "false"
-  action :logon_account
+  action :set_logon_account
 end
