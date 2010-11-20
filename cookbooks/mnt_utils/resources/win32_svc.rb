3af9950fa824feacc7d1126e2e743ac7549b9957
Ryan Geyer <rgeyer@its.jnj.com>
2010-11-19 16:30:37 -0800

    looks like Boolean isn't an exceptable type for a resource

--- cookbooks/mnt_utils/resources/win32_svc.rb
+++ cookbooks/mnt_utils/resources/win32_svc.rb
@@ -3,4 +3,4 @@ actions :set_logon_account
 attribute :service_name, :kind_of => [String]
 attribute :service_account_user, :kind_of => [String]
 attribute :service_account_pass, :kind_of => [String]
-attribute :restart_service, :kind_of => [Boolean]
+attribute :restart_service, :kind_of => [String]
