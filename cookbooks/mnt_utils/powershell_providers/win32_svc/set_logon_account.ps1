cb9d87198a0a27403d9521cc455d2e2329099b3d
Ryan Geyer <rgeyer@its.jnj.com>
2010-11-21 14:22:05 -0800

    Tweaks for service account settings

--- cookbooks/mnt_utils/powershell_providers/win32_svc/set_logon_account.ps1
+++ cookbooks/mnt_utils/powershell_providers/win32_svc/set_logon_account.ps1
@@ -8,9 +8,11 @@ $computer = get-content env:computername
 Write-Output("Service name is $service_name")
 Write-Output("restart service is $restart_service")
 
-# If service account user & pass are blank, we set it to $null, meaning that the Local System account will be used
+# The documentation says that "NULL" for StartName and StartPassword will result in Local System, but
+# it looks like you have to be explicit about it.
+# http://msdn.microsoft.com/en-us/library/aa384901(v=VS.85).aspx
 if ( ($service_account_user -eq "") -and ($service_account_pass -eq "") ) {
-  $service_account_user = $null
+  $service_account_user = "Local System"
   $service_account_pass = $null
 }
 
@@ -20,13 +22,16 @@ $service="name='$service_name'"
 
 Write-Output("The service WMI search is $service")
 
-$svc=Get-WmiObject win32_service -computer $computer -cred $Cred -filter $service
+$svc=Get-WmiObject win32_service -computer $computer -filter $service
 
 Write-Output("Service object is $svc")
 
 #if ($restart_service.ToLower() -eq "true") { $svc.StopService() }
 if ($svc) {
-  $svc.change($null,$null,$null,$null,$null,$null,$account,$password,$null,$null,$null)
+  $inParams = $svc.psbase.getMethodParameters("Change")
+  $inParams["StartName"] = $service_account_user
+  $inParams["StartPassword"] = $service_account_pass
+  $svc.invokeMethod("Change",$inParams,$null)
 }
 else { Write-Output("I no can getting the svc") }
 #if ($restart_service.ToLower() -eq "true") { $svc.StartService() }
